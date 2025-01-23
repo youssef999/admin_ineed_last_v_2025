77c01d4face686c06ff9676faed63a75f58b7141
Youssef Omar <youssef13579r@gmail.com>
2025-01-23 07:29:57 +0200

    app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a434
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2021716912"
+    serviceWorkerVersion: "2067512134"
   }
 });
